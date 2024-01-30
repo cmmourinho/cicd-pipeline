2dec2020f5c1d010bacd184ac3acd03273845b46
Tanaboon Chaichaur <tanaboon.cha@ttbbank.com>
2024-01-30 20:58:38 +0700

    add new pipeline

---
+++ jenkins2.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent {
+        label 'master'
+    }
+    
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git credentialsId: 'cmmourinho-github', url: 'https://github.com/cmmourinho/nodejs2.git', branch: 'main'
+            }
+        }
+
+        stage('Hello World') {
+            steps {
+                echo "Hello, World"
+            }
+        }
+    }
+}
