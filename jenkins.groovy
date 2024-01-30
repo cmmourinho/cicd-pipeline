9d5dd4fa21d7634ea5a24d02a4dce331fafd6ee5
Tanaboon Chaichaur <tanaboon.cha@ttbbank.com>
2024-01-30 20:44:30 +0700

    add stage

--- jenkins.groovy
+++ jenkins.groovy
@@ -4,6 +4,13 @@ pipeline {
     }
     
     stages {
+        stages {
+        stage('Clone Repository') {
+            steps {
+                git 'https://github.com/cmmourinho/nodejs.git'
+            }
+        }
+        
         stage('Hello World') {
             steps {
                 echo "Hello, World"
