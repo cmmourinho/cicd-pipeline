fc898fefe7c8c63b8cd21974fbd07c2ad3f0fe38
Tanaboon Chaichaur <tanaboon.cha@ttbbank.com>
2024-02-05 19:28:03 +0700

    add global config

--- test.groovy
+++ test.groovy
@@ -4,10 +4,18 @@ pipeline {
     agent any
 
     stages {
+        stage('prepare') {
+            steps {
+	        script {
+  		    checkout scm
+		    loadConfig = readYaml file: "./global-config.yaml"
+                    globalConfig = loadConfig["settings"]["global-settings"]
         stage('Thailand Greeting') {
             steps {
                 script {
                     test.thailand()
+ 		    echo "my docker url is ${globalConfig.general.dockerRepositoryUrl}"
+		    echo "my acr is ${acrNameToLogin}"
                 }
             }
         }
