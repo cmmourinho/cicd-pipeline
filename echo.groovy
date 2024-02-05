bef5fbd85a4dd7ece68ee5a45f1de365ce7ecc56
Tanaboon Chaichaur <tanaboon.cha@ttbbank.com>
2024-02-05 19:14:30 +0700

    delete hello world stage

--- echo.groovy
+++ echo.groovy
@@ -4,18 +4,10 @@ pipeline {
     agent any
 
     stages {
-        stage('Hello World') {
-            steps {
-                script {
-                    helloWorld()
-                }
-            }
-        }
-
         stage('Thailand Greeting') {
             steps {
                 script {
-                    thailand()
+                    echo.thailand()
                 }
             }
         }
